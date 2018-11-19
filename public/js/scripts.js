ed196e9e956905b63b98ab61b517691f5149cb97
Lauren Zou <zou.lauren@gmail.com>
2018-11-19 00:51:28 -0500

    Try out fullscreen.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,2 +1,8 @@
 const socket = io();
 new ViewController();
+
+$(document).ready(function() {
+  var root = document.documentElement    
+  root.requestFullscreen || root.webkitRequestFullscreen || root.mozRequestFullScreen ||
+    root.msRequestFullscreen;
+});
