d5ecfdafa6b64f9b2d57b0d658abb85e4197ff44
Lauren Zou <zou.lauren@gmail.com>
2018-11-19 01:14:17 -0500

    Add some checking UI.

--- public/js/components/white-card.js
+++ public/js/components/white-card.js
@@ -6,5 +6,25 @@ class WhiteCardComponent {
     this._innerElement = $('<div>').addClass('inner').text(content);
     $('<div>').addClass('logo').appendTo(this._innerElement);
     this.element = $('<div>').addClass('white-card').append(this._innerElement);
+
+    // Add checkbox.
+    $('<i>').addClass('fas fa-check').appendTo(this._innerElement);
+
+    // Add card order counter.
+    // TODO: only need this for multiple picks.
+    $('<div>').addClass('order-counter').text(1).appendTo(this._innerElement);
+
+    this._handleSelect();
+  }
+
+  /** Handles selecting the card. */
+  _handleSelect() {
+    this._innerElement.click(function() {
+      if ($(this).hasClass('selected')) {
+        $(this).removeClass('selected');
+      } else {
+        $(this).addClass('selected');
+      }
+    });
   }
 }
