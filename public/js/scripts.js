a66be4d86e30c1323d44eca09cb82d784fcabfb7
Lauren Zou <zou.lauren@gmail.com>
2018-11-19 00:55:20 -0500

    Remove the fullscreen thing.

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,8 +1,2 @@
 const socket = io();
 new ViewController();
-
-$(document).ready(function() {
-  var root = document.documentElement    
-  root.requestFullscreen || root.webkitRequestFullscreen || root.mozRequestFullScreen ||
-    root.msRequestFullscreen;
-});
